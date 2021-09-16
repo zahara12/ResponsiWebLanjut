2cb3c3133e32729b9e385a3e025c48093abad11c
unknown <ichx.zlm@gmail.com>
2021-09-16 14:11:09 +0700

    first commit

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <img src="/assets/images/linux.png" alt="logo" width="180" class="img-thumbnail rounded-circle">
+        <h1 class="display-4">Praktikum Web Lanjut Dengan Studi Kasus Blog App</h1>
+        <p class="lead">Programming | Codeigniter 4 | Php</p>
+    </div>
+</div>
---
+++ app/Views/v_home.php
@@ -0,0 +1,3 @@
+<div class="container mt-3">
+    <h1 class="text-center">Selamat Datang di Praktikum Web Lanjut</h1>
+</div>
